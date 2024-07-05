1b53ff9910067036dbfa0ebb89f965c116cf41b7
unknown <lsante@misena.edu.co>
2024-07-05 13:03:58 -0500

    mostrar la vista de la informacion del producto al usuario

--- src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.ecommerce.springecommerce.controller;
 
 import com.ecommerce.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    //variable que nos permite imprimir
+    private final Logger log= LoggerFactory.getLogger(HomeController.class);
     //objeto para obtener todos los productos
     @Autowired
     private ProductoService productoService;
@@ -18,5 +23,11 @@ public class HomeController {
         //pasamos 2 parametros nombre de la variable y pasamos todos lo productos
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+    //este metodo le permite ver la informacion del producto
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id) {
+        log.info("Id producto enviado como parametro.  {}", id);
+        return "usuario/productohome";
     }
 }
